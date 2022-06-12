82d9ac896e54a49ee1acc36228a5696cf32a2de5
Didi Ruhyadi <ruhyadi.dr@gmail.com>
2022-06-12 21:32:11 +0700

    success preds regressor

--- inference.py
+++ inference.py
@@ -1,7 +1,12 @@
 """ Inference Code """
 
+from typing import List
+from PIL import Image
+import cv2
+from glob import glob
+
 import torch
-from torch.utils.data import Dataset, DataLoader
+from torchvision.transforms import transforms
 from pytorch_lightning import LightningModule
 from src.utils import get_logger
 
@@ -27,7 +32,38 @@ def inference(config: DictConfig):
     regressor.load_state_dict(torch.load(config.get('regressor_weights')))
     regressor.eval()
 
+    # init preprocessing
+    log.info(f"Instantiating preprocessing")
+    preprocess: List[torch.nn.Module] = []
+    if 'augmentation' in config:
+        for _, conf in config.augmentation.items():
+            if '_target_' in conf:
+                preprocess.append(hydra.utils.instantiate(conf))
+    preprocess = transforms.Compose(preprocess)
     
+    # single image
+    # TODO: able inference on sequence images
+    imgs_path = sorted(glob(os.path.join(config.get('source_dir'), '*')))
+    for img_path in imgs_path:
+        name = img_path.split('/')[-1].split('.')[0]
+        img = Image.open(img_path)
+        # detect 2D bounding box
+        dets = [cv2.resize(det['im'], (224, 224), interpolation=cv2.INTER_CUBIC) \
+                for det in detector(img).crop()]
+        for i, det in enumerate(dets):
+            # preprocess img
+            det = preprocess(det)
+            # batching img
+            det = det.reshape((1, *det.shape))
+            res = regressor(det)
+            print(res)
+
+            if config.get('save_det2d'):
+                cv2.imwrite(f'{config.get("dump_dir")}/{name}_{i:03d}.png', det)
+
+    dets = detector(img).crop()
+    for i, det in enumerate(dets):
+        cv2.imwrite(f'/raid/didir/Repository/yolo3d-lightning/runs/sample_{i}.png', det['im'])
 
 def detector_yolov5(model_path: str, cfg_path: str, classes: int, device: str):
     """ YOLOv5 detector model """
